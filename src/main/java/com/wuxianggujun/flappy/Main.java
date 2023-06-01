2aef1efaacc0f6d502a7ed5a4b2191b7d719cdce
无相孤君 <39469560+wuxianggujun@users.noreply.github.com>
2023-06-01 17:01:02 +0800

    创建显示了一个全白的窗口

--- src/main/java/com/wuxianggujun/flappy/Main.java
+++ src/main/java/com/wuxianggujun/flappy/Main.java
@@ -1,10 +1,13 @@
 package com.wuxianggujun.flappy;
 
 import com.wuxianggujun.flappy.input.Input;
+import org.lwjgl.Version;
+import org.lwjgl.glfw.GLFWErrorCallback;
 import org.lwjgl.glfw.GLFWVidMode;
+import org.lwjgl.opengl.GL;
 
 import static org.lwjgl.glfw.GLFW.*;
-import static org.lwjgl.opengl.GL11.GL_TRUE;
+import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.system.MemoryUtil.*;
 
 public class Main implements Runnable {
@@ -18,29 +21,43 @@ public class Main implements Runnable {
     private long window;
 
     public void start() {
+        System.out.println("Hello LWJGL " + Version.getVersion() + "!");
         running = true;
         thread = new Thread(this, "Game");
         thread.start();
     }
 
     private void init() {
+        // Setup an error callback. The default implementation
+        // will print the error message in System.err.
+        GLFWErrorCallback.createPrint(System.err).set();
+
         if (!glfwInit()) {
             throw new IllegalStateException("无法初始化GLFW库！");
         }
 
-        glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
+        glfwDefaultWindowHints();
+        glfwWindowHint(GLFW_VISIBLE,GLFW_FALSE);//创建后窗口将保持隐藏状态
+        glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);//窗口可调整大小
+
         window = glfwCreateWindow(width, height, "Flappy", NULL, NULL);
         if (window == NULL) {
             throw new RuntimeException("无法创建窗口");
         }
 
+
         GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
         glfwSetWindowPos(window, (vidmode.width() - width) / 2, (vidmode.height() - height) / 2);
+
         glfwSetKeyCallback(window,new Input());
         glfwMakeContextCurrent(window);
         glfwShowWindow(window);
 
 
+        //获取当前OpenGL上下文的功能信息
+        GL.createCapabilities();
+        glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+        glEnable(GL_DEPTH_TEST);
     }
 
     @Override
@@ -61,9 +78,13 @@ public class Main implements Runnable {
 
     private void update() {
         glfwPollEvents();
+        if (Input.keys[GLFW_KEY_SPACE]){
+            System.out.println("Flap!");
+        }
     }
 
     private void render() {
+        glClear(GL_COLOR_BUFFER_BIT);
         glfwSwapBuffers(window);
     }
 
